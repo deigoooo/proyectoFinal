b361c0531022edd96d7623831826ea167df7f0eb
Diego Ramirez <df.ramirezz@gmail.com>
2023-08-28 14:53:15 -0300

    feat: actualizacion entrega clase 10

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 import express from "express";
+import handlebars from "express-handlebars";
 import productsRoutrer from "./routers/products.router.js";
 import cartsRouter from "./routers/carts.router.js";
+import realTimeProductsRouter from "./routers/realtimeproducts.router.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//configuro handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
+
 app.use("/api/products", productsRoutrer);
 app.use("/api/carts", cartsRouter);
+app.use("/api/realtimeproducts", realTimeProductsRouter);
 
 app.listen(8080, () => console.log("Server Running..."));
