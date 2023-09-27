a06e70d9b4e033b2e6528059968447327f3a297d
Diego Ramirez <df.ramirezz@gmail.com>
2023-09-26 22:09:11 -0300

    mejorando lineas basicas

--- src/socket.js
+++ src/socket.js
@@ -5,15 +5,15 @@ import MessageManager from "./dao/DB/messageManager.js";
 const message = new MessageManager();
 
 function initializeSocketIoServer(io) {
-  io.on("connection", (socket) => {
+  io.on("connection", async (socket) => {
+    io.emit("logs", await message.getMessage());
     console.log(`Nuevo cliente conectado: ${socket.id}`);
     socket.on("productList", (data) => {
       io.emit("updateProduct", data);
     });
     socket.on("message", async (data) => {
       message.addMessage(data);
-      const newMessage = await message.getMessage();
-      io.emit("logs", newMessage);
+      io.emit("logs", await message.getMessage());
     });
   });
 }
