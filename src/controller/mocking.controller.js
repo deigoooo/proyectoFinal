82d8f2ffc378715082d07edc2f3cc7b191cf4d4a
Diego Ramirez <df.ramirezz@gmail.com>
2023-12-11 20:51:54 -0300

    feat: endpoin / mockingproducts funcionando

--- src/controller/mocking.controller.js
+++ src/controller/mocking.controller.js
@@ -1,10 +1,26 @@
 import { faker } from "@faker-js/faker";
+import { productService } from "../services/Factory.js";
 
 export const getMockingController = async (req, res) => {
-  for (let index = 0; index < 100; index++) {
-    const element = array[index];
+  try {
+    const newProduct = [];
+    for (let index = 0; index < 100; index++) {
+      newProduct.push({
+        title: faker.commerce.product(),
+        description: faker.commerce.productDescription(),
+        price: faker.commerce.price({ min: 10, max: 1000, precision: 0.01 }),
+        thumbnail: [faker.image.url],
+        stock: faker.number.int({ min: 0, max: 1000 }),
+        code: faker.string.uuid(),
+        category: faker.commerce.department(),
+      });
+    }
+    const result = await productService.create(newProduct);
+    console.log(result);
+    res
+      .status(200)
+      .json({ status: "success", message: `The products was created` });
+  } catch (error) {
+    res.status(500).json({ status: "error", error: error.message });
   }
-  /* first_name: faker.person.firstName(),
-        last_name: faker.person.lastName(),
-        birth_date: faker.date.birthdate(), */
 };
