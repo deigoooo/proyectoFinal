56cfb11fb6f0ba83b2b27d05b8c285a1c2469086
Diego Ramirez <df.ramirezz@gmail.com>
2024-01-10 23:28:50 -0300

    feat: put de carritos funcionando

--- src/routers/carts.router.js
+++ src/routers/carts.router.js
@@ -14,13 +14,13 @@ import {
 const router = Router();
 
 router.get("/", getCartsController);
-router.get("/:cid", getCartController);
-router.get("/:cid/purchase", purchaseController);
-router.put("/:cid", updateCartController);
-router.put("/:cid/product/:pid", updateProductOnCartController);
 router.post("/", addCartController);
-router.post("/:cid/product/:pid", addProductToCartController);
+router.get("/:cid", getCartController);
+router.put("/:cid", updateCartController);
 router.delete("/:cid", deleteCartController);
+router.get("/:cid/purchase", purchaseController);
+router.put("/:cid/product/:pid", updateProductOnCartController);
+router.post("/:cid/product/:pid", addProductToCartController);
 router.delete("/:cid/product/:pid", deleteProductFromCart);
 
 export default router;
